5271e9a84db6a85639317e204eb145aa0b189236
Bill Ignition <valusvaloo@gmail.com>
2022-12-09 18:42:11 -0500

    Added Ftc Dashboard

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NativeTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NativeTest.java
@@ -29,11 +29,16 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import android.util.Log;
-
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.canvas.Canvas;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.geometry.Transform2d;
+import com.arcrobotics.ftclib.geometry.Translation2d;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.spartronics4915.lib.T265Camera;
 
 /**
  * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
@@ -48,17 +53,17 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@Autonomous(name="NativeTest1.07", group="Auto")
-public class NativeTest extends LinearOpMode {
+@Autonomous(name="NativeTest1.08", group="Auto")
+public class NativeTest extends OpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private NativeWrap nativeWrap = new NativeWrap();
-
+/*
     @Override
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
-        Log.i("Lookout7", "@@@@@@@@@@@@@@@@@@@@@@@  Running Op Mode  @@@@@@@@@@@@@@@@@@@@");
+        Log.i("Lookout", "@@@@@@@@@@@@@@@@@@@@@@@  Running Op Mode  @@@@@@@@@@@@@@@@@@@@");
         telemetry.update();
 
         nativeWrap.sayHello();
@@ -75,4 +80,54 @@ public class NativeTest extends LinearOpMode {
             telemetry.update();
         }
     }
+*/
+    // We treat this like a singleton because there should only ever be one object per camera
+    private static T265Camera slamra = null;
+
+    private final FtcDashboard dashboard = FtcDashboard.getInstance();
+
+    @Override
+    public void init() {
+        if (slamra == null) {
+            slamra = new T265Camera(new Transform2d(), 0.1, hardwareMap.appContext);
+        }
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+        slamra.start();
+    }
+
+    @Override
+    public void loop() {
+        final int robotRadius = 9; // inches
+
+        TelemetryPacket packet = new TelemetryPacket();
+        Canvas field = packet.fieldOverlay();
+
+        T265Camera.CameraUpdate up = slamra.getLastReceivedCameraUpdate();
+        if (up == null) return;
+
+        // We divide by 0.0254 to convert meters to inches
+        Translation2d translation = new Translation2d(up.pose.getTranslation().getX() / 0.0254, up.pose.getTranslation().getY() / 0.0254);
+        Rotation2d rotation = up.pose.getRotation();
+
+        field.strokeCircle(translation.getX(), translation.getY(), robotRadius);
+        double arrowX = rotation.getCos() * robotRadius, arrowY = rotation.getSin() * robotRadius;
+        double x1 = translation.getX() + arrowX  / 2, y1 = translation.getY() + arrowY / 2;
+        double x2 = translation.getX() + arrowX, y2 = translation.getY() + arrowY;
+        field.strokeLine(x1, y1, x2, y2);
+
+        dashboard.sendTelemetryPacket(packet);
+    }
+
+    @Override
+    public void stop() {
+        slamra.stop();
+    }
+
 }
