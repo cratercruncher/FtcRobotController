516ee2ebce131fdb0283567c9cbf5c4471850521
Bill Ignition <valusvaloo@gmail.com>
2022-12-30 22:19:38 -0500

    Updated Camera Test cpp

--- TeamCode/src/main/cpp/ftcrobotcontroller.cpp
+++ TeamCode/src/main/cpp/ftcrobotcontroller.cpp
@@ -1,6 +1,11 @@
 #include <jni.h>
 #include "include/librealsense2/rs.hpp"
 
+#include <android/log.h>
+#define  LOG_TAG    "testjni"
+
+#define  ALOG(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+
 // Write C++ code here.
 //
 // Do not forget to dynamically load the C++ library into your application.
@@ -22,6 +27,11 @@ extern "C"
 JNIEXPORT jint JNICALL
 Java_org_firstinspires_ftc_teamcode_Cameras_cameraCount(JNIEnv *env, jobject thiz) {
     // TODO: implement cameraCount()
+
     rs2::context ctx;
+
+
+    ALOG("This message comes from C at line %d.", __LINE__);
+
     return ctx.query_devices().size();
 }
