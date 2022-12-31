ad71819a4900c48c07c83ec9a78fa4e897f40f03
cratercruncher <cratercruncher@dragomight.us>
2022-12-30 21:41:51 -0500

    Fixing Camera OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTestOpMode.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+@TeleOp(name="Basic: CameraTestOpMode", group="Opmode")
+public class CameraTestOpMode extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    @Override
+    public void start(){
+
+    }
+
+    @Override
+    public void stop() {}
+}
