8f94220d9449e6f42b682f55c1c89973473633fd
cratercruncher <cratercruncher@dragomight.us>
2023-01-01 08:18:58 -0500

    Merged ftc265

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTestOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTestOpMode.java
@@ -12,20 +12,20 @@ public class CameraTestOpMode extends OpMode {
 
     @Override
     public void init() {
-        Cameras cameras = new Cameras();
-        int count = cameras.cameraCount();
-        Log.i("Camera", "Init Camera count is: " + count);
-        telemetry.addData("Init Camera count", count);
-        telemetry.update();
+//        Cameras cameras = new Cameras();
+//        int count = cameras.cameraCount();
+//        Log.i("Camera", "Init Camera count is: " + count);
+//        telemetry.addData("Init Camera count", count);
+//        telemetry.update();
     }
 
     @Override
     public void loop() {
-        Cameras cameras = new Cameras();
+       /* Cameras cameras = new Cameras();
         int count = cameras.cameraCount();
         Log.i("Camera", "Loop Camera count is: " + count);
         telemetry.addData("Loop Camera count", count);
-        telemetry.update();
+        telemetry.update();*/
     }
 
     @Override
